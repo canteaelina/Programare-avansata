a48cba507037e2478519d98ffb1fdd2b0bafadf3
Elina <nicoletacantea08@gmail.com>
2026-03-12 14:40:59 +0200

    .

---
+++ Lab3/src/lab3/Company.java
@@ -0,0 +1,55 @@
+package lab3;
+
+import java.util.HashMap;
+
+public class Company implements Profile {
+
+    String id;
+    String name;
+    private static int count = 0;
+    private HashMap<Profile, String> relationships;
+
+    public Company(String name)
+    {
+        relationships = new HashMap<>();
+        this.name = name;
+        id = "Company" + Integer.toString(count++);
+    }
+
+    @Override
+    public String getID()
+    {
+        return id;
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    //adaug o relatie
+    @Override
+    public void addRelationship(Profile p, String rel)
+    {
+        if(p != null)
+            relationships.put(p, rel);
+    }
+
+    @Override
+    public int compareTo(Profile other) {
+        return this.name.compareTo(other.getName());
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " - " + this.name;
+    }
+
+}
---
+++ Lab3/src/lab3/Person.java
@@ -0,0 +1,49 @@
+package lab3;
+
+import java.util.HashMap;
+
+public class Person implements Profile {
+    private String id;
+    private String name;
+    private static int count = 0;
+    private HashMap<Profile, String> relationships;
+
+    public Person(String name)
+    {
+        relationships = new HashMap<>();
+        this.name = name;
+        id = "Person" + Integer.toString(count++);
+    }
+
+    @Override
+    public String getID()
+    {
+        return id;
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public void addRelationship(Profile p, String rel)
+    {
+        relationships.put(p, rel);
+    }
+    @Override
+    public int compareTo(Profile other) {
+        return this.name.compareTo(other.getName());
+    }
+    @Override
+    public String toString() {
+        return this.id + " - " + this.name;
+    }
+}
